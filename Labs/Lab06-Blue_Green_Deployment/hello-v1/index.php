06f8dd695e35279f37c6e4df603c00286ce0629c
Nick Sanfratello <nsanfratello@Nicks-MacBook-Pro-2.local>
2016-07-13 15:47:25 -0400

    Modifying the Blue Green Lab as the previous one was not working.

---
+++ Labs/Lab06-Blue_Green_Deployment/hello-v1/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Hello world v1</title>
+    </head>
+    <body>
+          <p>Hello world <?php echo " version 1"; ?></p>
+    </body>
+</html>
---
+++ Labs/Lab06-Blue_Green_Deployment/hello-v2/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Hello world v2</title>
+    </head>
+    <body>
+          <p>Hello world <?php echo " version 2"; ?></p>
+    </body>
+</html>
