3e0e2f25870e2ed42a2818ab0402208c652e5279
nsanfratello <nsanfratello@pivotal.io>
2016-07-13 15:35:06 -0400

    Changed the name of the Helloworld application updated the code to reflect v2

--- Labs/Lab06-Blue_Green_Deployment/HelloWorld-v2.php
+++ Labs/Lab06-Blue_Green_Deployment/HelloWorld-v2.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
   <meta charset="utf-8">
-  <title>Hello world v1</title>
+  <title>Hello world v2</title>
   </head>
   <body>
   <p>Hello world <?php echo " version 2"; ?></p>
